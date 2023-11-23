f19ff1965386c1b3638449dfbdbc159032b53555
HAOTIAN LIU <shenglongzhihou1@gmail.com>
2023-11-24 00:54:04 +1000

    SONGS AND COLLECTION

--- song.py
+++ song.py
@@ -1,9 +1,17 @@
-"""..."""
-
-
-# TODO: Create your Song class in this file
-
+"""Song Class"""
 
 class Song:
-    """..."""
-    pass
+    def __init__(self, title, artist, year, is_learned=False):
+        self.title = title
+        self.artist = artist
+        self.year = year
+        self.is_learned = is_learned
+
+    def __str__(self):
+        return f"{self.title} by {self.artist} ({self.year}) {'*' if not self.is_learned else ''}"
+
+    def mark_learned(self):
+        self.is_learned = True
+
+    def mark_unlearned(self):
+        self.is_learned = False
--- songcollection.py
+++ songcollection.py
@@ -1,9 +1,30 @@
-"""..."""
+"""songcollection"""
 
-
-# TODO: Create your SongCollection class in this file
-
+import json
+from song import Song
 
 class SongCollection:
-    """..."""
-    pass
+    def __init__(self):
+        self.songs = []
+
+    def add_song(self, song):
+        self.songs.append(song)
+
+    def load_songs(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                for song_data in data:
+                    self.songs.append(Song(**song_data))
+        except FileNotFoundError:
+            pass  # Handle file not found
+
+    def save_songs(self, filename):
+        with open(filename, 'w') as file:
+            json.dump([song.__dict__ for song in self.songs], file)
+
+    def get_learned_songs(self):
+        return [song for song in self.songs if song.is_learned]
+
+    def get_unlearned_songs(self):
+        return [song for song in self.songs if not song.is_learned]
