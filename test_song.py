b61e9c7be2c615a075db230c3739224a728ed2a3
HAOTIAN LIU <shenglongzhihou1@gmail.com>
2023-11-24 16:24:09 +1000

    TEST

--- test_song.py
+++ test_song.py
@@ -1,24 +1,44 @@
-"""(Incomplete) Tests for Song class."""
+"""Tests for Song class."""
+
 from song import Song
 
-
 def run_tests():
-    """Test Song class."""
+    """
+    Run tests to verify the functionality of the Song class.
+    """
 
-    # Test empty song (defaults)
+    # Test creating a Song with default values
     print("Test empty song:")
     default_song = Song()
-    print(default_song)
+    print(default_song)  # Display the default song
+    # Assertions to ensure the song has default values
     assert default_song.artist == ""
     assert default_song.title == ""
     assert default_song.year == 0
     assert not default_song.is_learned
 
-    # Test initial-value song
+    # Test creating a Song with specified values
+    print("\nTest initial-value song:")
     initial_song = Song("My Happiness", "Powderfinger", 1996, True)
-    # TODO: Write tests to show this initialisation works
+    print(initial_song)  # Display the song with specified values
+    # Assertions to check if the song has the correct values
+    assert initial_song.artist == "Powderfinger"
+    assert initial_song.title == "My Happiness"
+    assert initial_song.year == 1996
+    assert initial_song.is_learned
 
-    # TODO: Add more tests, as appropriate, for each method
+    # Test toggling the learned status of a song
+    print("\nTest toggle_learned_status method:")
+    test_song = Song("Unlearned Song", "Unknown Artist", 2000)
+    print(f"Initial status: {test_song}")  # Display initial status
+    assert not test_song.is_learned  # Check if the song is initially unlearned
 
+    test_song.toggle_learned_status()  # Toggle the learned status
+    print(f"Status after learning: {test_song}")  # Display after toggling
+    assert test_song.is_learned  # Check if the song is learned
+
+    test_song.toggle_learned_status()  # Toggle the learned status again
+    print(f"Status after unlearning: {test_song}")  # Display after toggling again
+    assert not test_song.is_learned  # Check if the song is unlearned again
 
 run_tests()
--- test_songcollection.py
+++ test_songcollection.py
@@ -1,37 +1,39 @@
-"""(Incomplete) Tests for SongCollection class."""
+"""Tests for SongCollection class."""
+
 from song import Song
 from songcollection import SongCollection
 
-
 def run_tests():
-    """Test SongCollection class."""
+    """
+    Run tests to verify the functionality of the SongCollection class.
+    """
 
-    # Test empty SongCollection (defaults)
+    # Test 1: Creating an empty SongCollection and verifying it's empty
     print("Test empty SongCollection:")
-    song_collection = SongCollection()
-    print(song_collection)
-    assert not song_collection.songs  # an empty list is considered False
+    song_collection = SongCollection()  # Initialize an empty song collection
+    print(song_collection)  # Display the empty collection
+    assert not song_collection.songs  # Assert that the songs list is empty
 
-    # Test loading songs
+    # Test 2: Loading songs from a JSON file and verifying the collection is not empty
     print("Test loading songs:")
-    song_collection.load_songs('songs.json')
-    print(song_collection)
-    assert song_collection.songs  # assuming file is non-empty, non-empty list is considered True
+    song_collection.load_songs('songs.json')  # Load songs from the specified file
+    print(song_collection)  # Display the collection after loading songs
+    assert song_collection.songs  # Assert that the collection is not empty after loading
 
-    # Test adding a new Song with values
+    # Test 3: Adding a new song to the collection and verifying it is added
     print("Test adding new song:")
-    song_collection.add_song(Song("My Happiness", "Powderfinger", 1996, True))
-    print(song_collection)
+    song_collection.add_song(Song("My Happiness", "Powderfinger", 1996, True))  # Add a new song
+    print(song_collection)  # Display the collection after adding the song
 
-    # Test sorting songs
-    print("Test sorting - year:")
-    song_collection.sort("year")
-    print(song_collection)
-    # TODO: Add more sorting tests
+    # Test 4, 5, 6: Sorting the songs in the collection based on different attributes
+    # and verifying the sort functionality
+    for sort_key in ["year", "artist", "title"]:
+        print(f"Test sorting - {sort_key}:")
+        song_collection.sort(sort_key)  # Sort the collection based on the specified attribute
+        print(song_collection)  # Display the sorted collection
 
-    # TODO: Test saving songs (check file manually to see results)
-
-    # TODO: Add more tests, as appropriate, for each method
-
+    # Test 7: Saving the current state of the song collection to a file
+    print("Test saving songs:")
+    song_collection.save_songs('songs.json')  # Save the current state of the collection to a file
 
 run_tests()
